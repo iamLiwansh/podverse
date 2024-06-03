3ee886fdd4b651bf4e8fb22f8108c185a101fa64
iamLiwansh <saini.liwansh@gmail.com>
2024-06-03 16:52:31 +0530

    updated logo and titie of project

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import LeftSidebar from "@/components/LeftSidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -6,7 +8,7 @@ export default function RootLayout({
   return (
     <div>
         <main>
-            <p className="text-white-1">Left sidebar</p>
+           <LeftSidebar/>
             {children}
             <p className="text-white-1">Right sidebar</p>
         </main>
---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+
+const LeftSidebar = () => {
+  return (
+    <section className='left_sidebar'>
+        <nav className='flex flex-col gap-6'>
+            <Link href="/">
+                <Image src="/icons/logo.svg" alt='logo' width={23} height={27}/>
+            </Link>
+        </nav>
+    </section>
+  )
+}
+
+export default LeftSidebar
